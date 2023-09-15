52c97d55de09e6b387191854f073acf7c49adbea
InYzze <hueseyin.bagriacik@gmx.de>
2023-09-15 10:39:00 +0200

    Create index.php

---
+++ INA-IAV23/index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content=""width=device-width, inital-scale="1.0" \>
+        <title>Hueseyin Bagriacik</title>
+        <link rel="stylesheet" href="./../../assets/css/main.css" />
+    </head>
+    <body>
+        <header id="header">
+            <div class="inner">
+                <h1>Hueseyin Bagriacik</h1>
+                <h1><em>Vorstellung</em></h1>
+                <a><img src="./foto.jpg" width=150 height=150></a>
+            </div>
+        </header>
+        
+        <div id="main">
+
+            <p><b>Ich bin</b><br \>
+                <em><b>Hueseyin Bagriacik</b></em>
+            </p>
+
+            <p><b>Ich kann</b><br \>
+                <em>
+                </em>
+            </p>
+            <p><b>Ich will</b><br \>
+                <em>
+                </em>
+            </p>
+
+
+        </div>
+
+        <footer id="footer">
+            <div class="inner">
+                <ul class="icons">
+                </ul>
+                <ul class="copyright">
+                <li>&copy; Hueseyin Bagriacik</li>
+                </ul>
+            </div>
+        </footer>
+
+
+    </body>
+</html>
