3f9f7a2b0b52bf5a2c4f67f829ec1d37530d6fd1
InYzze <hueseyin.bagriacik@gmx.de>
2023-09-15 10:40:24 +0200

    Update index.php with personal data

--- INA-IAV23/index.php
+++ INA-IAV23/index.php
@@ -1,33 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="de">
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content=""width=device-width, inital-scale="1.0" \>
-        <title>Hueseyin Bagriacik</title>
+        <title>Hüseyin Bagriacik</title>
         <link rel="stylesheet" href="./../../assets/css/main.css" />
     </head>
     <body>
         <header id="header">
             <div class="inner">
-                <h1>Hueseyin Bagriacik</h1>
+                <h1>Hüseyin Bagriacik</h1>
                 <h1><em>Vorstellung</em></h1>
-                <a><img src="./foto.jpg" width=150 height=150></a>
+                <a><img src="./hbagriacik-profile-img.jpg" width=150 height=150></a>
             </div>
         </header>
         
         <div id="main">
 
             <p><b>Ich bin</b><br \>
-                <em><b>Hueseyin Bagriacik</b></em>
+                <em>Hüseyin Bagriacik</em><br \>
+                <em>Softwareentwickler (Fullstack) mit Schwerpunkt <a href=https://control-center.1und1.de>Control-Center</a></em><br \>
+                <em>Bei 1&1 Telecommunication SE beschäftigt</em>
             </p>
 
             <p><b>Ich kann</b><br \>
                 <em>
+                    In der Ausbildung zum Fachinformatiker für Anwendungsentwicklung (FIAE) habe ich unterschiedliche Einblicke in der Welte der Informationstechnik erhalten können.
+                    Zur meinem Arbeitsaltag gehören neben dem konstruieren, implementieren und der Prüfung des Quellcodes zusätzlich administrative Tätigkeiten.
+                    Linux ist mein ständiger Begleiter in meinem Altag.
                 </em>
             </p>
             <p><b>Ich will</b><br \>
                 <em>
+                    Ich will in die Welt der Informationstechnik weiter eintauchen, mein können festigen und neue Technologien sowie neue Programmierparadigmen erforschen.
+                    Am Ender der Ausbildung zum staatliche gepürften Informatiktechniker ist mein Ziel an mich selbst neben dem Wissensaustausch neuen Problemstellung besser zu anlyisieren und zu beheben.
                 </em>
             </p>
 
@@ -39,7 +45,7 @@
                 <ul class="icons">
                 </ul>
                 <ul class="copyright">
-                <li>&copy; Hueseyin Bagriacik</li>
+                <li>&copy; Hüseyin Bagriacik</li>
                 </ul>
             </div>
         </footer>
